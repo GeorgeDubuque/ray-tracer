aebf9bcff358f3b18880ae8ddfd6f1a9af0fd170
GeorgeDubuque <gtdubuque@gmail.com>
2025-12-05 18:34:04 -0800

    extracted initializing vars and rendering image into its own camera class

--- main.cpp
+++ main.cpp
@@ -1,36 +1,14 @@
 
-#include "interval.h"
+#include "camera.h"
 #include "rt_constants.h"
 
-#include "hittable.h"
 #include "hittable_list.h"
 #include "sphere.h"
 
 vec3 nice_pink =
     color(double(255 / 255.999), double(186 / 255.999), double(239 / 255.999));
 
-color ray_color(const ray &r, const hittable_list &world) {
-  hit_record hit_rec;
-  if (world.hit(r, interval(0, infinity), hit_rec)) {
-    return 0.5 * (hit_rec.normal + color(1, 1, 1));
-  }
-
-  // linear interp
-  // blendedVal = (1-a) * startValue + a * endValue
-  vec3 unit_direction = unit_vector(r.direction());
-  auto a = 0.5 * (unit_direction.y() + 1.0);
-  return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
-};
-
 int main() {
-
-  auto aspect_ratio = 16.0 / 9.0;
-
-  int image_width = 400;
-
-  int image_height = int(image_width / aspect_ratio);
-  image_height = image_height < 1 ? 1 : image_height;
-
   // World
   hittable_list world;
 
@@ -38,42 +16,10 @@ int main() {
   world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
   // Camera
-  auto focal_length = 1.0;
-  auto viewport_height = 2.0;
-  auto viewport_width = viewport_height * (double(image_width) / image_height);
+  camera cam;
 
-  auto camera_center = point3(0, 0, 0);
+  cam.aspect_ratio = 16.0 / 9.0;
+  cam.image_width = 400;
 
-  auto viewport_u = vec3(viewport_width, 0, 0);
-  auto viewport_v = vec3(0, -viewport_height, 0);
-
-  auto pixel_delta_u = viewport_u / image_width;
-  auto pixel_delta_v = viewport_v / image_height;
-
-  // need to move forward by focal length and then move
-  // to the top left of the viewport
-  auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) -
-                             viewport_u / 2 - viewport_v / 2;
-
-  // essentially getting the diaganol vector to the first pixel from the
-  // viewport top left to the first pixel and dividing in half
-  auto pixel00_loc =
-      viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-  std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
-  for (int j = 0; j < image_height; j++) {
-    std::clog << "\rScanlines remaining: " << (image_width - j);
-    for (int i = 0; i < image_width; i++) {
-      auto pixel_center =
-          pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-      auto ray_direction = pixel_center - camera_center;
-      ray r(camera_center, ray_direction);
-
-      color pixel_color = ray_color(r, world);
-      write_color(std::cout, pixel_color);
-    }
-  }
-
-  std::clog << "\rRender finished.                      \n";
+  cam.render(world);
 };
