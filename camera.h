9b9446b3e7532bd8efd88ba90efe56f31ff4e97e
GeorgeDubuque <gtdubuque@gmail.com>
2025-12-21 16:03:38 -0800

    adding positionable camera

--- camera.h
+++ camera.h
@@ -6,6 +6,7 @@
 #include "material.h"
 #include "rt_constants.h"
 #include "vec3.h"
+#include <cmath>
 
 class camera {
 public:
@@ -13,6 +14,11 @@ public:
   int image_width = 100;
   int samples_per_pixel = 10;
   int max_depth = 10;
+
+  double vfov = 90; // vertical view angle (fov)
+  point3 lookfrom = point3(0, 0, 0);
+  point3 lookat = point3(0, 0, -1);
+  vec3 vup = vec3(0, 1, 0); // cam relative up direction
 
   void render(const hittable &world) {
     initialize();
@@ -40,23 +46,31 @@ private:
   vec3 pixel_delta_u;
   vec3 pixel_delta_v;
   double pixel_sample_scale;
+  vec3 u, v, w;
 
   void initialize() {
 
     image_height = int(image_width / aspect_ratio);
     image_height = image_height < 1 ? 1 : image_height;
 
-    center = point3(0, 0, 0);
+    center = lookfrom;
 
     pixel_sample_scale = 1.0 / samples_per_pixel;
     // Camera
-    auto focal_length = 1.0;
-    auto viewport_height = 2.0;
+    auto focal_length = (lookfrom - lookat).length();
+    auto theta = degrees_to_radians(vfov);
+    auto h = std::tan(theta / 2);
+    auto viewport_height = 2.0 * h * focal_length;
     auto viewport_width =
         viewport_height * (double(image_width) / image_height);
 
-    auto viewport_u = vec3(viewport_width, 0, 0);
-    auto viewport_v = vec3(0, -viewport_height, 0);
+		w = unit_vector(lookfrom - lookat);
+		u = unit_vector(cross(vup, w));
+		v = cross(w, u);
+
+
+    auto viewport_u = viewport_width * u;
+    auto viewport_v = viewport_height * -v;
 
     pixel_delta_u = viewport_u / image_width;
     pixel_delta_v = viewport_v / image_height;
@@ -64,7 +78,7 @@ private:
     // need to move forward by focal length and then move
     // to the top left of the viewport
     auto viewport_upper_left =
-        center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+        center - (focal_length * w) - viewport_u / 2 - viewport_v / 2;
 
     // essentially getting the diaganol vector to the first pixel from the
     // viewport top left to the first pixel and dividing in half
